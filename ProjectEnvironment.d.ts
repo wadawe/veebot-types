379f44f4a77dff4fccf36a69ad1f5abc46438c31
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-20 14:12:53 +0100

    feat: add ProjectEnvironment

---
+++ ProjectEnvironment.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/ProjectEnvironment.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+export type ProjectEnvironment = "production" | "development" | "test";
