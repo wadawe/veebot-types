41c162116bb2893ba07eef0a8f73bcd739037753
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-20 14:12:01 +0100

    feat: add FormattedServerSettings.d.ts

---
+++ FormattedServerSettings.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/FormattedServerSettings.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+export interface FormattedServerSettings {
+    premium : boolean;
+    premiumExpires : string;
+    userCount : number;
+    currSystems : number;
+    maxSystems : number;
+}
