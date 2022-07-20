2a4652bbb15c85ad8b51b5bb9d3e9c50cbb15b14
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-20 14:12:16 +0100

    feat: add FormattedSystemChannels

---
+++ FormattedSystemChannels.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/FormattedSystemChannels.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+export interface FormattedSystemChannels {
+    systemNumber : number,
+    systemName : string,
+    createChannel : string;
+    parentCategory : string;
+}
