92556c4eb4389ce3aa1c09ec0a3b0aafe0715bda
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-20 14:11:36 +0100

    feat: add ErrorResponseContent

---
+++ ErrorResponseContent.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/ErrorResponseContent.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+export interface ErrorResponseContent {
+    response : string;
+    errors ?: string[];
+}
