7cee2e75544f3025893e28e053147d14d0dfc532
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-20 14:14:05 +0100

    feat: add UserServersResponse

---
+++ UserServersResponse.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/UserServersResponse.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+import { UserServerResponse } from "./UserServerResponse";
+
+export interface UserServersResponse {
+    [ key : string ] : UserServerResponse;
+}
