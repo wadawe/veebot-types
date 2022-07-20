31f57f4f6107a71102c9852a3d0694a4fc6fd78d
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-20 14:10:44 +0100

    feat: add DiscordsServerResponse

---
+++ DiscordsServerResponse.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/DiscordsServerResponse.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+export interface DiscordsServerResponse {
+    id : string;
+    name : string;
+    icon : string;
+    owner : boolean;
+    permissions : number;
+    permissions_new : string;
+}
