693e161c9ac94d7987f5fd7839b77aab8169e603
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-20 14:13:32 +0100

    feat: add SystemChannelsResponse

---
+++ SystemChannelsResponse.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/SystemChannelsResponse.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+import { FormattedSystemChannels } from "./FormattedSystemChannels";
+
+export interface SystemChannelsResponse {
+    settings : FormattedSystemChannels;
+}
