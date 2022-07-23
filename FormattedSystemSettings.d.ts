731c3f4c8521bf808aa38a14eaf94c650feec316
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-23 18:15:13 +0100

    edit: add defaults to FormattedSystemSettings

--- FormattedSystemSettings.d.ts
+++ FormattedSystemSettings.d.ts
@@ -10,8 +10,8 @@
 import { FormattedSystemChannels } from "./FormattedSystemChannels";
 
 export interface FormattedSystemSettings extends FormattedSystemChannels {
-    systemNumber : number,
-    systemName : string,
-    createChannel : string;
-    parentCategory : string;
+    defaultChannelName : string | null;
+    defaultChannelUserLimit : number | null;
+    defaultChannelBitrate : number | null;
+    defaultChannelState : boolean | null;
 }
