e5bd7b938656f111ad16a44ceb8ed9f37751121d
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-07-23 13:56:21 +0100

    feat: add SystemDefaultsResponse

---
+++ SystemDefaultsResponse.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/SystemDefaultsResponse.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+import { FormattedSystemDefaults } from "./FormattedSystemDefaults";
+
+export interface SystemDefaultsResponse {
+    settings : FormattedSystemDefaults;
+}
