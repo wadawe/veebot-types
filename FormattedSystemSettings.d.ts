2ed7bce9849cad28ea308a094db552f2a34e566d
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-08-07 10:24:08 +0100

    feat: add command toggles to FormattedSystemSettings

--- FormattedSystemSettings.d.ts
+++ FormattedSystemSettings.d.ts
@@ -10,8 +10,25 @@
 import { FormattedSystemChannels } from "./FormattedSystemChannels";
 
 export interface FormattedSystemSettings extends FormattedSystemChannels {
+
     defaultChannelName : string | null;
     defaultChannelUserLimit : number | null;
     defaultChannelBitrate : number | null;
     defaultChannelState : boolean | null;
+
+    enableCommandName : boolean | null;
+    enableCommandLimit : boolean | null;
+    enableCommandBitrate : boolean | null;
+    enableCommandLock : boolean | null;
+    enableCommandUnlock : boolean | null;
+    enableCommandGrant : boolean | null;
+    enableCommandDeny : boolean | null;
+    enableCommandClear : boolean | null;
+    enableCommandKick : boolean | null;
+    enableCommandMute : boolean | null;
+    enableCommandUnmute : boolean | null;
+    enableCommandTransfer : boolean | null;
+    enableCommandClaim : boolean | null;
+    enableCommandDelete : boolean | null;
+
 }
