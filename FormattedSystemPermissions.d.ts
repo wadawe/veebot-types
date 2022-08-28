819cced894deb53ff913a4302ad14ba69ce1636d
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-08-28 12:12:46 +0100

    feat: add FormattedSystemPermissions

---
+++ FormattedSystemPermissions.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/FormattedSystemPermissions.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+export interface FormattedSystemPermissions {
+    publicRoleId : string | null;
+}
