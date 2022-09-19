fdbb6ad1572c340dd287970f951bca5a2cc6e02f
Will Dawe <46680436+wadawe@users.noreply.github.com>
2022-09-19 10:31:44 +0100

    feat: add PermissionsKey

---
+++ PermissionsKey.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Typing File
+ * For storing relevant TypeScript types
+ *
+ * /types/PermissionsKey.d.ts
+ *
+ * Copyright (C) 2022 wadawe
+ */
+
+export type PermissionsKey = "lockedPerms" | "unlockedPerms" | "ownerPerms" | "grantedPerms" | "deniedPerms";
